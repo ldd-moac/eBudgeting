c63a7c2ee0ea5477d5db98a208650e3f5f5e6a0a
dbuaklee <dbuaklee@hotmail.com>
2013-12-27 07:33:14 +0700

    migrate to spring 4.0

--- eBudgeting/src/main/java/biz/thaicom/security/models/User.java
+++ eBudgeting/src/main/java/biz/thaicom/security/models/User.java
@@ -2,29 +2,21 @@ package biz.thaicom.security.models;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Set;
-
 import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Fetch;
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import javax.persistence.criteria.SetJoin;
-
 import org.springframework.data.jpa.domain.Specification;
 
 import biz.thaicom.eBudgeting.models.hrx.Organization;
