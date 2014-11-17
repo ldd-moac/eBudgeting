3a5fa27ae362cf3c7c4f3a7d63bbaf95820976d8
Det Buaklee <dbuaklee@hotmail.com>
2014-11-17 12:20:22 +0700

    fix misspell

--- eBudgeting/src/main/java/biz/thaicom/eBudgeting/services/EntityServiceJPA.java
+++ eBudgeting/src/main/java/biz/thaicom/eBudgeting/services/EntityServiceJPA.java
@@ -5537,7 +5537,7 @@ public class EntityServiceJPA implements EntityService {
 		}
 		
 		Long roundId = node.get("round").get("id").asLong();
-		OrganizationAllocationRound round = organizationAllocationRoundRepository.getOne(roundId);
+		OrganizationAllocationRound round = organizationAllocationRoundRepository.findOne(roundId);
 		
 		
 		Long budgetTypeId = node.get("budgetType").get("id").asLong();
